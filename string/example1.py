88242311116f249f19ad3f09cfe5f4de774adea2
hoysala <153725329+hoysala26@users.noreply.github.com>
2026-07-08 11:29:00 +0530

    small change

--- string/example1.py
+++ string/example1.py
@@ -1,6 +1,12 @@
-s = "Hello World"
+s = "  Hello World  "
 
- 
+print(s.strip())          
+print(s.lower())          
+print(s.upper())          
+print(s.replace("World", "Python") ) 
+print(s.split() )         
+print(s.split(","))       
+"-".join(["a","b","c"])  
 print(s.startswith("He"))
 print(s.endswith("ld"))
 print(s.find("World"))    
